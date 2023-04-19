a95861a9961b41b29d1bcf2ba978760c2dbf376d
Igor Flores Bento <81481911+IgorFlores2003@users.noreply.github.com>
2023-04-18 21:57:33 -0300

    Add files via upload

---
+++ Prova_01/java.js
@@ -0,0 +1,13 @@
+function nome(){
+        const name = document.getElementById("nome")
+        const Sobre=document.getElementById("Sobrenome")
+        const nomes = document.createElement("h1")
+        nomes.innerHTML=name.value
+          
+
+        const container = document.getElementById("container")
+        container.append(nomes,)
+    }
+    
+
+  
